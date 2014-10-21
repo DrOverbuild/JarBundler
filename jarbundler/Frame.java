5c6fa919e1ad071feaeb15673faecd09a3806a59
Jasper Reddin <jasper@192.168.1.137>
2014-10-20 21:02:21 -0500

    Added log screen that shows user what's going on.

--- jarbundler/Frame.java
+++ jarbundler/Frame.java
@@ -27,13 +27,16 @@
 package jarbundler;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Desktop;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
@@ -42,6 +45,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileFilter;
@@ -53,6 +58,7 @@ import javax.swing.filechooser.FileFilter;
 public class Frame extends JFrame{
 
 	public static final String newline = System.getProperty("line.separator");
+	public static final String ANT_INSTALLATION = "/usr/share/java/ant";
 
 	File outputDirectoryFile;
 	File jarfileFile;
@@ -64,11 +70,17 @@ public class Frame extends JFrame{
 	public JTextField mainClassNameField;
 	public JTextField iconFileFeild;
 	public JTextField versionStringField;
-
+	public JTextArea outputArea;
+	
+	Runnable bundleThread;
+	
 	public Frame(){
 		super("Jar Bundler");
-		setLayout(new GridLayout(6, 1));
-
+		setLayout(new BorderLayout());
+		
+		bundleThread = () -> bundle();
+		
+		JPanel panel8 = new JPanel(new GridLayout(5,1));
 		JPanel panel1 = new JPanel(new BorderLayout());
 		panel1.setBorder(new EmptyBorder(2, 2, 2, 2));
 		outputDirectoryField = new JTextField(15);
@@ -96,7 +108,7 @@ public class Frame extends JFrame{
 		panel1.add(new JLabel("Save output file as: "), BorderLayout.WEST);
 		panel1.add(outputDirectoryField, BorderLayout.CENTER);
 		panel1.add(chooseButton, BorderLayout.EAST);
-		add(panel1);
+		panel8.add(panel1);
 
 		JPanel panel3 = new JPanel(new BorderLayout());
 		panel3.setBorder((new EmptyBorder(2, 2, 2, 2)));
@@ -124,14 +136,14 @@ public class Frame extends JFrame{
 		panel3.add(new JLabel("Jar file: "), BorderLayout.WEST);
 		panel3.add(jarFileField, BorderLayout.CENTER);
 		panel3.add(openButton, BorderLayout.EAST);
-		add(panel3);
+		panel8.add(panel3);
 
 		JPanel panel4 = new JPanel(new BorderLayout());
 		panel4.setBorder(new EmptyBorder(2, 2, 2, 2));
 		mainClassNameField = new JTextField(15);
 		panel4.add(new JLabel("Main class: "), BorderLayout.WEST);
 		panel4.add(mainClassNameField, BorderLayout.CENTER);
-		add(panel4);
+		panel8.add(panel4);
 
 		JPanel panel5 = new JPanel(new BorderLayout());
 		panel5.setBorder((new EmptyBorder(2, 2, 2, 2)));
@@ -159,21 +171,32 @@ public class Frame extends JFrame{
 		panel5.add(new JLabel("Icon: "), BorderLayout.WEST);
 		panel5.add(iconFileFeild, BorderLayout.CENTER);
 		panel5.add(openButton2, BorderLayout.EAST);
-		add(panel5);
+		panel8.add(panel5);
 
 		JPanel panel6 = new JPanel(new BorderLayout());
 		panel6.setBorder(new EmptyBorder(2, 2, 2, 2));
 		versionStringField = new JTextField(15);
 		panel6.add(new JLabel("Version String: "), BorderLayout.WEST);
 		panel6.add(versionStringField, BorderLayout.CENTER);
-		add(panel6);
+		panel8.add(panel6);
 
 		JPanel panel7 = new JPanel(new BorderLayout());
 		JButton buildButton = new JButton("Bundle");
-		buildButton.addActionListener((ActionEvent e) -> bundle());
-		panel7.add(buildButton, BorderLayout.CENTER);
+		buildButton.addActionListener((ActionEvent e) -> new Thread(bundleThread).start());
+		panel7.add(buildButton, BorderLayout.NORTH);
+		outputArea = new JTextArea(4,30);
+		outputArea.setFont(new Font("Monospaced",Font.PLAIN,12));
+		outputArea.setForeground(Color.WHITE);
+		outputArea.setBackground(Color.BLACK);
+		outputArea.setLineWrap(true);
+		outputArea.setEditable(false);
+		JScrollPane scrollPane = new JScrollPane(outputArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		panel7.add(scrollPane,BorderLayout.CENTER);
 		add(panel7);
 
+		add(panel8, BorderLayout.CENTER);
+		add(panel7, BorderLayout.SOUTH);
+		
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setVisible(true);
 		pack();
@@ -207,7 +230,7 @@ public class Frame extends JFrame{
 			"    <description>Temporary build config for ant script</description>" +newline+
 			"        <taskdef name=\"bundleapp\"" +newline+
 			"            classname=\"com.oracle.appbundler.AppBundlerTask\""+newline+
-			"            classpath=\"/usr/share/java/ant/lib/appbundler-1.0.jar\" />"+newline+
+			"            classpath=\"" + ANT_INSTALLATION + "/lib/appbundler-1.0.jar\" />"+newline+
 			"        <target name=\"bundle-app\">"+newline+
 			"            <bundleapp outputdirectory=\"" + outputDirectory.getParent() + "\"" +newline+
             "                name=\""+name+"\"" +newline+
@@ -226,12 +249,28 @@ public class Frame extends JFrame{
 	}
 
 	private void bundle(){
+		outputArea.setText("");
+		
 		if(outputDirectoryFile == null || jarfileFile == null || iconFile == null){
-			JOptionPane.showMessageDialog(null, "Error: Empty text fields.", "Error", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Please fill in all the text fields.", "", JOptionPane.ERROR_MESSAGE);
 			return;
 		}
+				
+		enterText("Checking for Ant...");
+		if(!new File(ANT_INSTALLATION).exists()){
+			enterText("[Error] Ant was not found at " + ANT_INSTALLATION);
+			return;
+		}
+		
+		if (!new File(ANT_INSTALLATION + "/lib/appbundler-1.0.jar").exists()){
+			enterText("[Error] appbundler-1.0.jar not found at ANT_INSTALLATION/lib");
+			return;
+		}
+		
+
 
 		// Create build.xml file
+		enterText("Generating build.xml...");
 		File parent = outputDirectoryFile.getParentFile();
 		File buildDotXML = new File(parent, "build.xml");
 		BufferedWriter writer = null;
@@ -253,29 +292,45 @@ public class Frame extends JFrame{
 				}
 			}
 		}
-
+		
 		// Bundle the app
+		enterText("Running Ant...");
 		Process bundleApp;
 		try {
-			bundleApp = Runtime.getRuntime().exec("/usr/share/java/ant/bin/ant bundle-app", new String[]{},parent);
-			bundleApp.waitFor();
-		} catch (IOException | InterruptedException ex) {
-			Logger.getLogger(Frame.class.getName()).log(Level.SEVERE, null, ex);
-		}
+			bundleApp = Runtime.getRuntime().exec(ANT_INSTALLATION + "/bin/ant bundle-app", new String[]{},parent);
+			Scanner s = new Scanner(bundleApp.getInputStream());
+			while (s.hasNext()){
+				try{
+					String next = s.nextLine();
+					enterText(next);
+				}catch (java.util.NoSuchElementException e){
+				}
+			}
 
-		// Delete build.xml
-		buildDotXML.delete();
+			enterText("Deleting build.xml...");
+			// Delete build.xml
+			buildDotXML.delete();
 
-		// Notify user process was complete
-		JOptionPane.showMessageDialog(null, "Bundle was complete!");
+			enterText("Done");
+			
+			enterText("Opening parent folder in default file browser");
 
-		// Show bundled app in file browser
-		try {
-			Desktop.getDesktop().open(parent);
+			// Show bundled app in file browser
+			try {
+				Desktop.getDesktop().open(parent);
+			} catch (IOException ex) {
+				Logger.getLogger(Frame.class.getName()).log(Level.SEVERE, null, ex);
+			}
+
 		} catch (IOException ex) {
 			Logger.getLogger(Frame.class.getName()).log(Level.SEVERE, null, ex);
 		}
 
+	}
+	
+	public void enterText(String txt){
+		outputArea.setText(outputArea.getText() + txt + System.getProperty("line.separator"));
+		outputArea.setCaretPosition(outputArea.getText().length()-1);
 	}
 
 }
